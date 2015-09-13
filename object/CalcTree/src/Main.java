03acf8be9a7f32c2af055d5c48e49bee1ccec340
willian ver valen <vervalenpaiva@gmail.com>
2015-09-13 16:50:11 +0200

    main

---
+++ object/CalcTree/src/Main.java
@@ -0,0 +1,15 @@
+import Tree.Tree;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("test"); 
+        Tree t2 = new Tree("(-5+5)*3/6-9+25");
+        System.out.println(t2.toString());
+        System.out.println(t2.solve());
+        //String myString= "125.05+25-98*7/3";
+        //String[] result = myString.split("(?<=[-+*/])|(?=[-+*/])");
+        //System.out.println(Arrays.toString(result));
+    }
+}
